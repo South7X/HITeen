c6847d9b839776ae9ab65476efc6d64fab61a6a9
Brendan-Bu <2576557900@qq.com>
2020-10-19 01:42:11 +0800

    v1.2

--- app/src/main/java/com/hitsz/eatut/StatisticsActivity.java
+++ app/src/main/java/com/hitsz/eatut/StatisticsActivity.java
@@ -1,28 +1,38 @@
 package com.hitsz.eatut;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.hitsz.eatut.ui.Search_ui.SearchFragment;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import androidx.fragment.app.Fragment;
+import com.example.popupwindowlibrary.view.ScreenPopWindow;
 import com.hitsz.eatut.AAChartCoreLib.AAChartCreator.AAChartModel;
 import com.hitsz.eatut.AAChartCoreLib.AAChartCreator.AASeriesElement;
 import com.hitsz.eatut.AAChartCoreLib.AAChartEnum.AAChartType;
-
+import com.example.popupwindowlibrary.bean.FiltrateBean;
 import java.util.ArrayList;
+import java.util.List;
 
-public class StatisticsActivity extends AppCompatActivity{
+public class StatisticsActivity extends AppCompatActivity {
+    private ScreenPopWindow screenPopWindow;
+    private List<FiltrateBean> expenseList = new ArrayList<>();
+    private List<FiltrateBean> favorList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        Activity statis=this;
         setContentView(R.layout.activity_statistics);
         Button time = (Button) findViewById(R.id.time);
         Button expense = (Button) findViewById(R.id.expense);
         Button favor = (Button) findViewById(R.id.favor);
         Button explore = (Button) findViewById(R.id.explore);
         com.hitsz.eatut.AAChartCoreLib.AAChartCreator.AAChartView aaChartView = findViewById(R.id.AAChartView);
+        initParam();
         AAChartModel aaChartModel = new AAChartModel()
                 .chartType(AAChartType.Column)
                 .title("THE HEAT OF PROGRAMMING LANGUAGE")
@@ -55,16 +65,34 @@ public class StatisticsActivity extends AppCompatActivity{
         });
         expense.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                aaChartModel.chartType=AAChartType.Area;
-                aaChartView.aa_drawChartWithChartModel(aaChartModel);
+            public void onClick(View view){
+                screenPopWindow = new ScreenPopWindow(statis, expenseList);
+                //设置多选，因为共用的一个bean，这里调用reset重置下数据
+                screenPopWindow.setSingle(false).reset().build();
+                screenPopWindow.showAsDropDown(expense);
+                screenPopWindow.setOnConfirmClickListener(new ScreenPopWindow.OnConfirmClickListener() {
+                    @Override
+                    public void onConfirmClick(List<String> list) {
+                        aaChartModel.chartType = AAChartType.Line;
+                        aaChartView.aa_drawChartWithChartModel(aaChartModel);
+                    }
+                });
             }
         });
         favor.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                aaChartModel.chartType=AAChartType.Line;
-                aaChartView.aa_drawChartWithChartModel(aaChartModel);
+            public void onClick(View view){
+                screenPopWindow = new ScreenPopWindow(statis, favorList);
+                //设置多选，因为共用的一个bean，这里调用reset重置下数据
+                screenPopWindow.setSingle(false).reset().build();
+                screenPopWindow.showAsDropDown(favor);
+                screenPopWindow.setOnConfirmClickListener(new ScreenPopWindow.OnConfirmClickListener() {
+                    @Override
+                    public void onConfirmClick(List<String> list) {
+                            aaChartModel.chartType = AAChartType.Line;
+                            aaChartView.aa_drawChartWithChartModel(aaChartModel);
+                    }
+                });
             }
         });
         explore.setOnClickListener(new View.OnClickListener(){
@@ -74,5 +102,32 @@ public class StatisticsActivity extends AppCompatActivity{
                 aaChartView.aa_drawChartWithChartModel(aaChartModel);
             }
         });
+    }
+    private void initParam() {
+        String[] filter = {"周","月","年"};
+        String[] sort = {"口味偏好","档口偏好"};
+
+        FiltrateBean fb1 = new FiltrateBean();
+        fb1.setTypeName("查看类型");
+        List<FiltrateBean.Children> childrenList0 = new ArrayList<>();
+        for (String afilter : filter) {
+            FiltrateBean.Children cd = new FiltrateBean.Children();
+            cd.setValue(afilter);
+            childrenList0.add(cd);
+        }
+        fb1.setChildren(childrenList0);
+
+        FiltrateBean fb2 = new FiltrateBean();
+        fb2.setTypeName("查看类型");
+        List<FiltrateBean.Children> childrenList1 = new ArrayList<>();
+        for (String asort : sort) {
+            FiltrateBean.Children cd = new FiltrateBean.Children();
+            cd.setValue(asort);
+            childrenList1.add(cd);
+        }
+        fb2.setChildren(childrenList1);
+
+        expenseList.add(fb1);
+        favorList.add(fb2);
     }
 }
